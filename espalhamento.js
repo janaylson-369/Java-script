e92c72ce11f62a40e21434b04175dad9972d3e72
Janailson Gomes <janailson.gomes@academico.ifpb.edu.br>
2026-04-07 16:38:06 -0300

    Update espalhamento.js

--- espalhamento.js
+++ espalhamento.js
@@ -14,3 +14,13 @@ console.log(valores2);
 
 //atribuição por copia - modelo 2
 valores2 = [...valores1];
+
+
+//estudar array map, para o mini test
+
+// com desestruturação
+let exibir_aluno_matricola = ({nome, matricola}) => {
+  console.log(`ola ${nome} sua matricola: ${matricola}`)
+};
+
+exibir_aluno_matricola(aluno1);
